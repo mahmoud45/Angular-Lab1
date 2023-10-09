d4eff2daba907fc3f32ae336649f6d7b501c9916
mahmoud45 <magdymahmoud913@gmail.com>
2023-10-10 00:29:04 +0300

    adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/header/navbar.component';
+import { HeroComponent } from './Components/Sections/hero/hero.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { CategoryComponent } from './Components/Sections/category/category.component';
+import { CategoryItemComponent } from './Components/Sections/category/category-item/category-item.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavbarComponent,
+        HeroComponent,
+        FooterComponent,
+        CategoryComponent,
+        CategoryItemComponent,
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
